7e6e7c14ab8c225b91494dfaee339f7afb197e06
Alan Bielik <abielik92@gmail.com>
2020-08-30 15:30:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,12 +16,5 @@ if (string.toUpperCase() === string) {return "YES INDEED!"};
 if (string.toLowerCase() === string) {return "I can't hear you!"};
 if (string === "I love you, Grandma.") {return "I love you, too."};
 
-var uppercase = "I LOVE YOU, GRANDMA.";
-var lowercase = "i love you, grandma.";
-var mixedCase = "I love you, Grandma.";
 
-uppercase.toUpperCase() === uppercase;
-lowercase.toLowerCase() === lowercase;
-mixedCase.toUpperCase() === mixedCase;
-mixedCase.toLowerCase() === mixedCase;
 }
