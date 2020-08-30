83064c48cd578972d7e0e81725aacf996c41d4a6
Alan Bielik <abielik92@gmail.com>
2020-08-30 15:30:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-if ("I LOVE YOU, GRANDMA.") {return "YES INDEED!"};
-if ("i love you, grandma.") {return "I can't hear you!"};
-if ("I love you, Grandma.") {return "I love you, too."};
+if (string.toUpperCase() === string) {return "YES INDEED!"};
+if (string.toLowerCase() === string) {return "I can't hear you!"};
+if (string === "I love you, Grandma.") {return "I love you, too."};
 
 var uppercase = "I LOVE YOU, GRANDMA.";
 var lowercase = "i love you, grandma.";
